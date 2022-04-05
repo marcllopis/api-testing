100c9149893fe4e24ffee0ede2cefc67add2f3b4
Marc <marcllopis89@gmail.com>
2022-04-05 12:34:23 +0200

    api test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [githubData, setGithubData] = useState({});
+  let [starWarsData, setStarWarsData] = useState({});
+  let [apiLoaded, setApiLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/marcllopis")
+      .then((response) => response.json())
+      .then((data) => setGithubData(data));
+  }, []);
+
+  const starWarsCall = () => {
+    fetch("https://swapi.dev/api/people/")
+      .then((response) => response.json())
+      .then((data) => {
+        setStarWarsData(data);
+        setApiLoaded(true);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Api Calls</h1>
+      <h3>Github api stuff</h3>
+      <p>{githubData.name}</p>
+      <p>{githubData.location}</p>
+      <p>{githubData.url}</p>
+      <hr />
+      <hr />
+      <h3>Star Wars API stuff</h3>
+      <button onClick={starWarsCall}>Give me Star Wars</button>
+      {
+        // apiLoaded &&
+        //   starWarsData.results.map(function (item) {
+        //     let newItem = item.name;
+        //     return <p>{newItem}</p>;
+        //   })
+        // apiLoaded && starWarsData.results.map((item) => <p>{item.name}</p>)
+        apiLoaded &&
+          starWarsData.results.map(function (item, index) {
+            return (
+              <div key={index}>
+                <h2>{item.name}</h2>
+                <h3>{item.gender}</h3>
+                <p>
+                  {item.height}cm and {item.mass} kg
+                </p>
+                <br />
+              </div>
+            );
+          })
+      }
     </div>
   );
 }
